6ea3aecb35357cea64280cb895ae69e88bb2560a
aomerk <aliomerkeser@gmail.com>
2021-05-10 21:00:28 +0200

    Initial cmd code

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package cmd
+
+/*
+	Created by aomerk at 5/10/21 for project pot
+*/
+
+/*
+	INSERT FILE DESCRIPTION HERE
+*/
+
+// global constants for file
+const ()
+
+// global variables (not cool) for this file
+var ()
+
+func main() {
+
+}
